3621bc34154139938f87d7497a397fe69094651c
Sven Rhinow <sven@sr-tag.de>
2016-06-21 14:17:30 +0200

    V 0.5

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,19 +1,6 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
-
+<?php
 /**
- * TYPOlight webCMS
- *
- * The TYPOlight webCMS is an accessible web content management system that 
- * specializes in accessibility and generates W3C-compliant HTML code. It 
- * provides a wide range of functionality to develop professional websites 
- * including a built-in search engine, form generator, file and user manager, 
- * CSS engine, multi-language support and many more. For more information and 
- * additional TYPOlight applications like the TYPOlight MVC Framework please 
- * visit the project website http://www.typolight.org.
- *
- * This file modifies the data container array of table tl_module.
- *
- * @copyright  Sven Rhinow 2011
+ * @copyright  Sven Rhinow 2016
  * @author     Sven Rhinow <sven@sr-tag.de>
  * @package    randomNumber
  * @license    LGPL
@@ -30,6 +17,3 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['m2u_jumpTo'] = array(
 		'inputType'               => 'pageTree',
 		'eval'                    => array('fieldType'=>'radio')
 	);
-
-
-?>
